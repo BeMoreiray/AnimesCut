a23fcd72b925146ee95aaddacbbd652f91b12bc2
Beatriz Moreira Silva <99758926+BeMoreiray@users.noreply.github.com>
2023-06-01 19:32:56 -0300

    User's getters and setters

--- animescut/src/main/java/br/com/project/animescut/entities/User.java
+++ animescut/src/main/java/br/com/project/animescut/entities/User.java
@@ -5,4 +5,32 @@ public class User {
 	private String fullName;
 	private String email;
 	private String password;
+	
+	
+	public Integer getIdUser() {
+		return idUser;
+	}
+	public void setIdUser(Integer idUser) {
+		this.idUser = idUser;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 }
