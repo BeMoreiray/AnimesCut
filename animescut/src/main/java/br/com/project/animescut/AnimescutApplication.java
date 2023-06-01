e6e2d7d1694359aeccb48e7d99a4ab0da4ac8b0a
Beatriz Moreira Silva <99758926+BeMoreiray@users.noreply.github.com>
2023-06-01 16:19:44 -0300

    project created

---
+++ animescut/src/main/java/br/com/project/animescut/AnimescutApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.animescut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimescutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimescutApplication.class, args);
+	}
+
+}
---
+++ animescut/src/test/java/br/com/project/animescut/AnimescutApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.animescut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimescutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
