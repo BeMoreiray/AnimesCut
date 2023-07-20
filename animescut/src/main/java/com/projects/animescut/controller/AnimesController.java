ee8036ffa9b3380a690f868a9167c8eb8225a357
Beatriz Moreira Silva <99758926+BeMoreiray@users.noreply.github.com>
2023-07-20 18:38:05 -0300

    Fetching anime by ID

--- animescut/src/main/java/com/projects/animescut/controller/AnimesController.java
+++ animescut/src/main/java/com/projects/animescut/controller/AnimesController.java
@@ -4,19 +4,27 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projects.animescut.entities.Animes;
 import com.projects.animescut.repositories.AnimesRepository;
 
 @RestController
+@RequestMapping(value = "/animes")
 public class AnimesController {
 	@Autowired 
 	private AnimesRepository repository;
 	 
-	@GetMapping(value = "/animes")
+	@GetMapping
 	public List<Animes> getAllAnimes(){
 		return repository.findAll();
+	}
+	
+	@GetMapping(value = "/{id}")
+	public Animes getById(@PathVariable Long id) {
+		return repository.findById(id).get();
 	}
 
 }
