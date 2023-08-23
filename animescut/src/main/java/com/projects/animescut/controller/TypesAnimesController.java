49918fa94cc5ece4c77e68c77cd34e5358e788c9
Beatriz Moreira Silva <99758926+BeMoreiray@users.noreply.github.com>
2023-08-23 19:00:20 -0300

    new method: getTypesAnimesById

--- animescut/src/main/java/com/projects/animescut/controller/TypesAnimesController.java
+++ animescut/src/main/java/com/projects/animescut/controller/TypesAnimesController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,12 @@ public class TypesAnimesController {
 	@GetMapping
 	public List<TypesAnimes> getAllTypes(){
 		List<TypesAnimes> result = service.findAll();
+		return result;
+	}
+	
+	@GetMapping(value="/{id}")
+	public TypesAnimes getTypesById(@PathVariable Long id) {
+		result = service.findById(id);
 		return result;
 	}
 	
--- animescut/src/main/java/com/projects/animescut/services/TypesAnimesService.java
+++ animescut/src/main/java/com/projects/animescut/services/TypesAnimesService.java
@@ -1,6 +1,8 @@
 package com.projects.animescut.services;
 
+
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,7 +12,7 @@ import com.projects.animescut.entities.TypesAnimes;
 import com.projects.animescut.repositories.TypesAnimesRepository;
 
 @Service
-@Validated
+@Validated //anotação utilizada para auxiliar @notnull e @notBlank, permite que a validação seja feita
 public class TypesAnimesService {
 	
 	@Autowired
@@ -21,5 +23,17 @@ public class TypesAnimesService {
 		return result;
 	}
 	
-
+	public TypesAnimes findById(Long id) {
+		Optional<TypesAnimes> optionalTypesAnimes = repository.findById(id);
+		if(optionalTypesAnimes.isPresent()) {
+			return optionalTypesAnimes.get();
+		}else {
+			throw new ResourceNotFoundException("Categoria de anime não encontrada com o ID informado! Tente novamente");
+		}
+	}
+	
+	public TypesAnimes insertNewObject(TypesAnimes types) {
+		TypesAnimes result = repository.save(types);
+		return result;
+	}
 }
