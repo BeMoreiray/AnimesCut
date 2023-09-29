fc4ee154da7ce6b4d3716913c0a50883ce73ca04
Beatriz Moreira Silva <99758926+BeMoreiray@users.noreply.github.com>
2023-09-29 13:24:55 -0300

    Classes MoviesService + MoviesController completed

--- animescut/src/main/java/com/projects/animescut/controller/SeriesController.java
+++ animescut/src/main/java/com/projects/animescut/controller/SeriesController.java
@@ -12,10 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.projects.animescut.entities.Animes;
 import com.projects.animescut.entities.Series;
 import com.projects.animescut.services.SeriesService;
 
