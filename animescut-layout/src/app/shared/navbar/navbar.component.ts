c2e88f3a6166a42c8f8d8c13e0aef9f9a77632c7
Beatriz Moreira Silva <99758926+BeMoreiray@users.noreply.github.com>
2023-10-19 16:17:23 -0300

    navbar completed

--- animescut-layout/src/app/shared/navbar/navbar.component.ts
+++ animescut-layout/src/app/shared/navbar/navbar.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {NgFor} from '@angular/common';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
   standalone: true,
-  imports: [MatToolbarModule]
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatSelectModule, MatFormFieldModule]
 })
 export class NavbarComponent {
 
