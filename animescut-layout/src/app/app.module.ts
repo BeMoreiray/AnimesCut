b88cdbe6fbcc907699420727df90b6268b34a1d0
Beatriz Moreira Silva <99758926+BeMoreiray@users.noreply.github.com>
2023-10-23 19:36:20 -0300

    new angular structure

--- animescut-layout/src/app/app.module.ts
+++ animescut-layout/src/app/app.module.ts
@@ -4,22 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
-
-import { SharedModule } from './shared/shared.module';
-import { AuthModule } from './auth/auth.module';
-
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    SharedModule,
-    AuthModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
